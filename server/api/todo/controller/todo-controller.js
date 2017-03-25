984e2983ce2c9898199f1caf8282356b3f634729
Anabelle Handdoek <ana@gathacol.net>
2017-03-25 00:29:06 -0500

    return only one

--- server/api/todo/controller/todo-controller.js
+++ server/api/todo/controller/todo-controller.js
@@ -6,7 +6,7 @@ module.exports = class TodoController {
   static getAll(req, res) {
       TodoDAO
         .getAll()
-        .then(todos => res.status(200).json(todos[0]))
+        .then(todos => res.status(200).json(todos[todos.length - 1]))
         .catch(error => res.status(400).json(error));
   }
 
